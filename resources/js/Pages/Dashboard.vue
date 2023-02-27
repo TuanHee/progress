c61463931a7575f1f7f251bf1aacda70b0b4f103
Tuan Hee Teo <tuanhee@marslab.com.my>
2023-02-27 09:37:22 +0800

    doughnut chart

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -8,30 +8,12 @@
 
         <div class="pt-4 py-3">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <h3 class="mb-2 font-semibold">Projects Recent</h3>
+                <div class="flex">
+                    <h3 class="mb-2 font-semibold">Projects Recent</h3>
+                </div>
                 <div class="grid grid-cols-2 lg:grid-cols-4 gap-4">
                     <div class="h-20 text-center col-span-2 lg:col-span-4" v-if="in_projects.length == 0">Not Project yet</div>
-                    <Link :href="route('projects.show', { project : project.id })" class="card min-h-full" v-for="project in in_projects" :key="project.id">
-                        <h5 class="font-semibold">{{ project.title }}</h5>
-                        <div class="pb-2 flex justify-between items-center">
-                            <div class="flex text-gray-500 items-center"
-                                :class="{
-                                    'text-green-500': project.tasks_completed_count == project.tasks_count && project.tasks_count != 0
-                                }">
-                                <CheckCircleIcon class="w-5 h-5" />
-                                <span class="ml-1">{{ (project.tasks_count != 0) ? project.tasks_completed_count + "/" + project.tasks_count : "-" }}</span>
-                            </div>
-                            <div class="flex justify-end pl-1 -space-x-1">
-                                <profile-photo :src="member.profile_photo_url" :alt="member.name" v-for="member in project.joined_members.slice(0,3)" :size="7" :key="member.id" />
-                                <div v-if="project.all_joined_members_count > 3" class="flex rounded-full justify-center text-purple-600 bg-purple-100 items-center h-8 w-8">
-                                    <span>
-                                        ...
-                                    </span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="text-right text-xs text-gray-600">{{ project.created_at }}</div>
-                    </Link>
+                    <project-card v-for="project in in_projects" :key="project" :project="project" />
                 </div>
             </div>
         </div>
@@ -69,15 +51,13 @@
 <script>
     import { Link } from '@inertiajs/inertia-vue3'
     import AppLayout from '@/Layouts/AppLayout.vue'
-    import ProfilePhoto from "@/Shared/ProfilePhoto.vue";
-    import { CheckCircleIcon } from '@heroicons/vue/outline'
+    import ProjectCard from '@/Shared/ProjectCard.vue'
 
     export default {
         components: {
             Link,
             AppLayout,
-            ProfilePhoto,
-            CheckCircleIcon,
+            ProjectCard,
         },
         props: {
             in_tasks: Array,
---
+++ resources/js/Shared/ProjectCard.vue
@@ -0,0 +1,87 @@
+<template>
+    <Link :href="route('projects.show', { project : project.id })" class="card min-h-full">
+        <div class="pb-2 flex justify-between">
+            <div class="flex flex-col space-y-1">
+                <h5 class="font-semibold">{{ project.title }}</h5>
+                <div class="flex text-gray-500 items-center"
+                    :class="{
+                        'text-green-500': project.tasks_completed_count == project.tasks_count && project.tasks_count != 0
+                    }">
+                    <CheckCircleIcon class="w-5 h-5" />
+                    <span class="ml-1">{{ (project.tasks_count != 0) ? project.tasks_completed_count + "/" + project.tasks_count : "-" }}</span>
+                </div>
+                <div class="flex -space-x-1">
+                    <profile-photo :src="member.profile_photo_url" :alt="member.name" v-for="member in project.joined_members.slice(0,3)" :size="7" :key="member.id" />
+                    <div v-if="project.all_joined_members_count > 3" class="flex rounded-full justify-center text-purple-600 bg-purple-100 items-center h-8 w-8">
+                        <span>
+                            ...
+                        </span>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <canvas :id="'chart-'+ project.id" class="w-1/2"></canvas>
+            </div>
+        </div>
+        <div class="text-right text-xs text-gray-600">{{ project.created_at }}</div>
+    </Link>
+</template>
+
+<script>
+    import { Link } from '@inertiajs/inertia-vue3'
+    import Chart from 'chart.js/auto'
+    import ProfilePhoto from '@/Shared/ProfilePhoto.vue'
+    import { CheckCircleIcon } from '@heroicons/vue/outline'
+
+    export default {
+        components: {
+            Link,
+            Chart,
+            ProfilePhoto,
+            CheckCircleIcon,
+        },
+
+        props: {
+            project: Object,
+        },
+
+        mounted() {
+            const ctx = document.getElementById('chart-'+ this.project.id)
+
+
+            const data = this.project.tasks_count != 0 ? [
+                { type: 'completed', count: this.project.tasks_completed_count },
+                { type: 'incompleted', count: this.project.tasks_count - this.project.tasks_completed_count },
+            ] : [
+                { type: 'Empty', count: 1 },
+            ]
+
+            const backgroundColors = this.project.tasks_count != 0 ? [
+                'rgb(0, 0, 132)',
+                'rgb(205, 205, 205)',
+            ] : [
+                'rgb(205, 205, 205)',
+            ]
+
+            new Chart(ctx, {
+                type: 'doughnut',
+                options: {
+                    plugins: {
+                        legend: {
+                            display: false
+                        },
+                    }
+                },
+                data: {
+                    labels: data.map(row => row.type),
+                    datasets: [
+                        {
+                            data: data.map(row => row.count),
+                            backgroundColor: backgroundColors
+                        }
+                    ]
+                }
+            });
+        }
+    }
+</script>
